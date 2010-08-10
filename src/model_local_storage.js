97af306341c07e0034115e8b36e9ab0302075b35
Ben Pickles <spideryoung@gmail.com>
2010-08-10 22:13:35 +0100

    Manage the localStorage collection when creating, updating and destroying models.

--- src/model_local_storage.js
+++ src/model_local_storage.js
@@ -1,25 +1,57 @@
 Model.LocalStorage = function(klass) {
+  var collection_uid = [klass._name, "collection"].join("-")
+  var readIndex = function() {
+    var data = localStorage[collection_uid]
+    return data ? JSON.parse(data) : []
+  }
+  var writeIndex = function(uids) {
+    localStorage.setItem(collection_uid, JSON.stringify(uids))
+  }
+  var addToIndex = function(uid) {
+    var uids = readIndex()
+
+    if (uids.indexOf(uid) === -1) {
+      uids.push(uid)
+      writeIndex(uids)
+    }
+  }
+  var removeFromIndex = function(uid) {
+    var uids = readIndex()
+    var index = uids.indexOf(uid)
+
+    if (index > -1) {
+      uids.splice(index, 1)
+      writeIndex(uids)
+    }
+  }
+  var store = function(model) {
+    var uid = model.uid,
+       data = JSON.stringify(model.attr())
+    localStorage.setItem(uid, data)
+    addToIndex(uid)
+  }
+
   return {
     create: function(model, callback) {
-      this.store(model)
+      store(model)
       callback(true)
     },
 
     destroy: function(model, callback) {
       localStorage.removeItem(model.uid)
+      removeFromIndex(model.uid)
       callback(true)
     },
 
     read: function(callback) {
       if (!callback) return false
 
-      var uid = [klass._name, "collection"].join("-")
-      var records = JSON.parse(localStorage[uid])
+      var uids = readIndex()
       var models = []
       var attributes, model, uid
 
-      for (var i = 0, length = records.length; i < length; i++) {
-        uid = records[i]
+      for (var i = 0, length = uids.length; i < length; i++) {
+        uid = uids[i]
         attributes = JSON.parse(localStorage[uid])
         model = new klass(attributes)
         model.uid = uid
@@ -29,14 +61,8 @@ Model.LocalStorage = function(klass) {
       callback(models)
     },
 
-    store: function(model) {
-      var uid = model.uid,
-         data = JSON.stringify(model.attr())
-      localStorage.setItem(uid, data)
-    },
-
     update: function(model, callback) {
-      this.store(model)
+      store(model)
       callback(true)
     }
   }
--- test/tests/model_local_storage.js
+++ test/tests/model_local_storage.js
@@ -10,15 +10,19 @@ var localStorageTest = function(name, func) {
 localStorageTest("read", function() {
   var Post = Model("post", { persistence: Model.LocalStorage })
 
+  Post.persistence.read(function(models) {
+    equals(models.length, 0)
+  })
+
   localStorage.setItem("post-a", JSON.stringify({ a: "a" }))
   localStorage.setItem("post-b", JSON.stringify({ b: "b" }))
   localStorage.setItem("post-collection", '["post-a","post-b"]')
 
-  Post.persistence.read(function(data) {
-    equals(data.length, 2)
+  Post.persistence.read(function(models) {
+    equals(models.length, 2)
 
-    var post1 = data[0]
-    var post2 = data[1]
+    var post1 = models[0]
+    var post2 = models[1]
 
     equals("post-a", post1.uid)
     same({ a: "a" }, post1.attr())
@@ -31,24 +35,45 @@ localStorageTest("create, update, destroy", function() {
   var Post = Model("post", { persistence: Model.LocalStorage })
 
   equals(Post.count(), 0)
+  equals(localStorage.length, 0)
 
   var post = new Post({ title: "foo", foo: "bar" })
   post.save()
 
   equals(Post.count(), 1)
-  equals(localStorage.length, 1)
+  equals(localStorage.length, 2)
   same({ title: "foo", foo: "bar" }, JSON.parse(localStorage[post.uid]))
+  equals(localStorage["post-collection"], '["' + post.uid + '"]',
+    "should be stored in localStorage")
+
+  Post.persistence.read(function(models) {
+    equals(models.length, 1)
+    same(models[0].attr(), post.attr())
+  })
 
   post.attr({ title: ".", foo: null, bar: "baz" })
   post.save()
 
   equals(Post.count(), 1)
-  equals(localStorage.length, 1)
+  equals(localStorage.length, 2)
   same({ title: ".", foo: null, bar: "baz" }, JSON.parse(localStorage[post.uid]))
+  equals(localStorage["post-collection"], '["' + post.uid + '"]',
+    "should not alter localStorage list")
+
+  Post.persistence.read(function(models) {
+    equals(models.length, 1)
+    same(models[0].attr(), post.attr())
+  })
 
   post.destroy()
 
   equals(Post.count(), 0)
-  equals(localStorage.length, 0)
+  equals(localStorage.length, 1)
   ok(localStorage[post.uid] === undefined)
+  equals(localStorage["post-collection"], "[]",
+    "should be removed from localStorage list")
+
+  Post.persistence.read(function(models) {
+    equals(models.length, 0)
+  })
 })
