8b65ac544f2a8fe4ce4ce34841f28e68fa1955c7
Ben Pickles <spideryoung@gmail.com>
2012-02-26 21:53:30 +0000

    Generate a new Collection sub-class for each model so that custom collection methods can be defined per-model.

--- src/model.js
+++ src/model.js
@@ -1,11 +1,18 @@
 var Model = function(name, func) {
   var model = Model.Model.extend()
-
   model._name = name
-  model.collection = new Model.Collection()
+
+  // Generate a model-specific Collection subclass.
+  var Collection = model.Collection = Model.Collection.extend()
+
+  // Assign a default collection to the model and have it auto add/remove a
+  // model on save/destroy.
+  model.collection = new Collection()
   model.collection.listenTo(model.anyInstance)
 
-  if (Model.Utils.isFunction(func)) func.call(model, model, model.prototype)
+  if (Model.Utils.isFunction(func)) {
+    func.call(model, model, model.prototype, Collection.prototype)
+  }
 
   return model;
 };
